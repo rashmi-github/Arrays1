1df5239c5b54dc698faefa9da886c4cdeeb436ac
Rashmi <email2Rashmeet@gmail.com>
2023-04-12 14:11:04 -0700

    test

---
+++ src/arrays/Array1.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+  int[] x= new int[2];
+  x[0]=10;
+ // x[0.2]=20;
+  System.out.println(x);
+  
+ //// int[] v= new int[-5];
+  
+  int [][] m= new int [2][];
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rashmi
+ *
+ */
+module arrays {
+}
